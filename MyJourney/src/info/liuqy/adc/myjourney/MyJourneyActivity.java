43eb7fac812d17c6fab1f44228eb99f6be0f7140
Evan Liu <qingyan123@gmail.com>
2012-05-14 22:06:52 +0800

    add some class vars.

--- MyJourney/src/info/liuqy/adc/myjourney/MyJourneyActivity.java
+++ MyJourney/src/info/liuqy/adc/myjourney/MyJourneyActivity.java
@@ -1,18 +1,32 @@
 package info.liuqy.adc.myjourney;
 
+import java.util.List;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
 
 public class MyJourneyActivity extends MapActivity {
+    MapView mapView;
+    MapController mapCtrl;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    List<Overlay> mapOverlays;
+    MyLocationOverlay myLocationOverlay;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
 
     }
