7c666d141968112be709bc267a74a3235ddbb6f4
Evan Liu <qingyan123@gmail.com>
2012-05-15 00:44:48 +0800

    new VideoPlayer extends Activity

---
+++ MyJourney/src/info/liuqy/adc/myjourney/VideoPlayer.java
@@ -0,0 +1,37 @@
+package info.liuqy.adc.myjourney;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+
+public class VideoPlayer extends Activity implements Callback,
+		OnCompletionListener {
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
