559c1e7b7ac755a3e0d38700561e100556b29e43
Evan Liu <qingyan123@gmail.com>
2012-05-14 22:17:48 +0800

    source|implement dispatchTap

--- MyJourney/src/info/liuqy/adc/myjourney/MyMyLocationOverlay.java
+++ MyJourney/src/info/liuqy/adc/myjourney/MyMyLocationOverlay.java
@@ -6,10 +6,20 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 
 public class MyMyLocationOverlay extends MyLocationOverlay {
+    private Context context;
+    private MapView mapView;
 
 	public MyMyLocationOverlay(Context context, MapView mapView) {
 		super(context, mapView);
-		// TODO Auto-generated constructor stub
+		
+        this.context = context;
+        this.mapView = mapView;
+	}
+
+	@Override
+	protected boolean dispatchTap() {
+		// TODO Auto-generated method stub
+		return super.dispatchTap();
 	}
 
 }
