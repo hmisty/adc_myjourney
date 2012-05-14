fd48835a465e91559550de60ef51f720cc0d7f4e
Evan Liu <qingyan123@gmail.com>
2012-05-14 22:35:13 +0800

    new FootprintOverlay, skeleton

---
+++ MyJourney/src/info/liuqy/adc/myjourney/FootprintOverlay.java
@@ -0,0 +1,27 @@
+package info.liuqy.adc.myjourney;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class FootprintOverlay extends ItemizedOverlay<OverlayItem> {
+
+	public FootprintOverlay(Drawable defaultMarker) {
+		super(defaultMarker);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
