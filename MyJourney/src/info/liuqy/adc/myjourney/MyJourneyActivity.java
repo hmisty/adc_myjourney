92761aaa1a8fc9f097b99c7120f822610875197d
Evan Liu <qingyan123@gmail.com>
2012-05-14 22:01:01 +0800

    change the main activity to extends MapActivity

--- MyJourney/src/info/liuqy/adc/myjourney/MyJourneyActivity.java
+++ MyJourney/src/info/liuqy/adc/myjourney/MyJourneyActivity.java
@@ -1,13 +1,20 @@
 package info.liuqy.adc.myjourney;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class MyJourneyActivity extends Activity {
+import com.google.android.maps.MapActivity;
+
+public class MyJourneyActivity extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
