5f42ad4b939b252684cfde002b6f2bf4358b8db0
Evan Liu <qingyan123@gmail.com>
2012-05-14 22:16:51 +0800

    create MyMyLocationOverlay extends MyLocationOverlay

---
+++ MyJourney/src/info/liuqy/adc/myjourney/MyMyLocationOverlay.java
@@ -0,0 +1,15 @@
+package info.liuqy.adc.myjourney;
+
+import android.content.Context;
+
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+
+public class MyMyLocationOverlay extends MyLocationOverlay {
+
+	public MyMyLocationOverlay(Context context, MapView mapView) {
+		super(context, mapView);
+		// TODO Auto-generated constructor stub
+	}
+
+}
