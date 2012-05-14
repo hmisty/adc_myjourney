d2490ad9ce1aaa5f9f2aa394070b8e6f8b2705b8
Evan Liu <qingyan123@gmail.com>
2012-05-15 00:46:37 +0800

    source|implement onCreate()

--- MyJourney/src/info/liuqy/adc/myjourney/VideoPlayer.java
+++ MyJourney/src/info/liuqy/adc/myjourney/VideoPlayer.java
@@ -1,8 +1,12 @@
 package info.liuqy.adc.myjourney;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
@@ -14,6 +18,28 @@ public class VideoPlayer extends Activity implements Callback,
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
     String uriString;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.videoplayer);
+
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setFixedSize(176, 144);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDisplay(surfaceHolder);
+        mediaPlayer.setOnCompletionListener(this);
+        
+        Intent i = this.getIntent();
+        uriString = i.getStringExtra("uri");        
+	}
 
 	@Override
 	public void onCompletion(MediaPlayer mp) {
