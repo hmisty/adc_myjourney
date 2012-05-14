ac9760c0d6c36b600c34b1de15297e96eb9c15b8
Evan Liu <qingyan123@gmail.com>
2012-05-14 22:27:47 +0800

    implement dispatchTap to show a dialog with 3 choices

--- MyJourney/src/info/liuqy/adc/myjourney/MyMyLocationOverlay.java
+++ MyJourney/src/info/liuqy/adc/myjourney/MyMyLocationOverlay.java
@@ -1,25 +1,43 @@
 package info.liuqy.adc.myjourney;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
 
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 
 public class MyMyLocationOverlay extends MyLocationOverlay {
-    private Context context;
-    private MapView mapView;
+	private Context context;
+	private MapView mapView;
 
 	public MyMyLocationOverlay(Context context, MapView mapView) {
 		super(context, mapView);
-		
-        this.context = context;
-        this.mapView = mapView;
+
+		this.context = context;
+		this.mapView = mapView;
 	}
 
 	@Override
 	protected boolean dispatchTap() {
-		// TODO Auto-generated method stub
-		return super.dispatchTap();
+		AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+		dialog.setTitle(R.string.tap_dialog_title);
+		dialog.setSingleChoiceItems(R.array.tap_dialog_choices, -1,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int item) {
+						switch (item) {
+						default:
+							Toast.makeText(
+									context,
+									context.getResources().getStringArray(
+											R.array.tap_dialog_choices)[item],
+									Toast.LENGTH_SHORT).show();
+						}
+					}
+				});
+		dialog.show();
+		return true;
 	}
 
 }
